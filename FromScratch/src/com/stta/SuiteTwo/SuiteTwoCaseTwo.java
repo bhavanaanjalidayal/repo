517e9db0fc829deec65bc9e29d32d5d96f055db6
Bhavana Anjali Dayal <anjalidayal1812@gmail.com>
2018-08-29 20:24:11 +0530

    First commit

---
+++ FromScratch/src/com/stta/SuiteTwo/SuiteTwoCaseTwo.java
@@ -0,0 +1,10 @@
+package com.stta.SuiteTwo;
+
+public class SuiteTwoCaseTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ FromScratch/src/com/stta/utility/SuiteUtility.java
@@ -0,0 +1,10 @@
+package com.stta.utility;
+
+public class SuiteUtility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
